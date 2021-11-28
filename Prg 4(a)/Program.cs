8b6ac1358991836c1951dc1fa28dd3ab016b9357
Muhammad Hamza <ghulamabbas2247@gmail.com>
2021-11-28 14:20:14 +0500

    Prg 4

---
+++ Prg 4(a)/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Prg_4_a_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=3, b=4;
+            a= ++a*b++;
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Prg 4(b)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Prg_4_b_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=3, b=4;
+            a=++a*++b;
+            Console.WriteLine(a);
+        }
+    }
+}
